2b8ccd83e1c6c88886b477d28d464c162034c222
Jason nguyen <jasonvex@gmail.com>
2012-07-25 16:56:28 -0500

    Still working on Problem 11

--- ProjectEuler/Problem11.rb
+++ ProjectEuler/Problem11.rb
@@ -1,2 +1,24 @@
 #author: Jason Nguyen (jason.z.nguyen@gmail.com)
 #@jonasguynen
+require 'Matrix'
+
+file = File.open("C:/erms_workspace/RubyPractice/ProjectEuler/input/Problem11.in","r")
+
+num = ""
+i=0
+matrix = Array.new
+file.each_line do|line|
+  #puts "Reading this line: #{line}"
+  #print line.split.map(&:to_i)
+  matrix [i]=line.split.map(&:to_i)
+  i=i+1
+  #puts
+end
+
+for r in 0..matrix.length
+    for c in 0..matrix[r].length
+      print "#{matrix[r][c]} "
+    end
+    puts
+    
+end
